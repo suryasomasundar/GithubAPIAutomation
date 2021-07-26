a37f030d027a9dbfb4ac0099004f4c0aca34baf5
Somu <ssuryanarayanan@myvest.com>
2021-07-26 14:53:28 -0700

    APIAutomation_for_Github_WS

---
+++ APIAutomation_RestAssured/src/test/java/com/webservice/utils/PropertyReader.java
@@ -0,0 +1,16 @@
+package com.webservice.utils;
+
+public class PropertyReader {
+
+    public final String username = "suryasomasundar";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public final String accessToken = "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";
+}
